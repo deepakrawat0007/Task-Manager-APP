05bd15858ecd4ff4f0260547302d526fd3037ec6
deepakrawat0007 <deepakrawat3738@gmail.com>
2023-04-20 21:29:17 +0530

    Background Image Fix

--- Task-Manager/src/login.jsx
+++ Task-Manager/src/login.jsx
@@ -46,7 +46,7 @@ const Login = () => {
 	return (
 		<>
 			<div className="limiter">
-				<div className="container-login100" style={{ backgroundImage: "url(images/bg-01.jpg)" }}>
+				<div className="container-login100" style={{ backgroundImage: "url(/bg-01.jpg)" }}>
 					<div className="wrap-login100">
 						<form className="login100-form validate-form">
 							<span className="login100-form-logo">
