00addbcfff0254d766d3e7e0a22c3cad68d6f0a9
deepakrawat0007 <deepakrawat3738@gmail.com>
2023-04-21 18:47:58 +0530

    UI-Changes

--- Task-Manager/src/dashBoard.jsx
+++ Task-Manager/src/dashBoard.jsx
@@ -3,10 +3,10 @@ import Profile from "./profile";
 
 const DashBoard = () =>{
     return (
-        <>
+        <div className="container-login100" style={{ backgroundImage: "url(/bg-01.jpg)", margin:"0",padding:"0" }}>
         <NavBar/>
         <Profile/>
-        </>
+        </div>
     )
 }
 export default DashBoard;
--- Task-Manager/src/navBar.jsx
+++ Task-Manager/src/navBar.jsx
@@ -8,22 +8,22 @@ const NavBar = () => {
   }
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav class="navbar navbar-expand-lg navbar-light new-col navBar-new-style">
         <div class="container-fluid">
-          <span class="navbar-brand">Task Manager</span>
+          <span class="navbar-brand" style={{color:"white"}}>Task Manager</span>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <span class="nav-link active" style={{ cursor: "pointer" }} aria-current="page" onClick={() => navigate("/home")}>Home</span>
+                <span class="nav-link active" style={{ cursor: "pointer", color:"white" }} aria-current="page" onClick={() => navigate("/home")}>Home</span>
               </li>
               <li class="nav-item">
-                <span class="nav-link" style={{ cursor: "pointer" }} onClick={() => navigate("/tasks")}>Tasks</span>
+                <span class="nav-link" style={{ cursor: "pointer", color:"white" }} onClick={() => navigate("/tasks")}>Tasks</span>
               </li>
               <li class="nav-item">
-                <button class="nav-link" style={{ cursor: "pointer" }} onClick={handleLogout}>Logout</button>
+                <button class="nav-link" style={{ cursor: "pointer", color:"white" }} onClick={handleLogout}>Logout</button>
               </li>
 
 
